0d2c734d90426521ff369a58964c5e977c0a684c
Marco <marco.nextime@gmail.com>
2024-10-25 01:13:56 +0100

    Update de Mensagem

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 server.on('connection', (socket) => {
-  console.log('Client connected');
-  
-  socket.on('message', (message) => {
-    console.log('Received:', message);
-    socket.send(`Echo: ${message}`);
-  });
-  
-  socket.on('close', () => {
-    console.log('Client disconnected');
-  });
+    console.log('Client connected');
+
+    socket.on('message', (message) => {
+        console.log('Received:', message);
+        
+        // Envia um objeto JSON como resposta
+        const response = { type: 'echo', message: message };
+        socket.send(JSON.stringify(response));
+    });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
 });
 
 console.log('WebSocket server is running...');
